946739acccff78551473d2d23a78c4dffbf0ebda
smoreno19 <smoreno19@my.whitworth.edu>
2016-12-04 09:57:12 -0800

    updated Dealing function and did a little tweek in other places

--- UNO/UNO/Player.h
+++ UNO/UNO/Player.h
@@ -1,17 +1,21 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 #include <iostream>
+#include "Cards.h"
 #include <string>
 using namespace std;
 class Player {
 private:
+	Cards** cards;
+	int numcards;
+	int CurrentCards;
 	string username;
 	int PlayerHand;
 public:
 	Player(string pName);
 	string getUsername();
 	int getHand();
-	void addToHand();
+	void addToHand(Cards* addCard);
 	void playFromHand();
 };
 #endif // !PLAYER_H
